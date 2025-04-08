90b137e76d0d64a65cfb0dbfe5ee3d043e7eaa8c
Shashidhar Biradar <shashidharb459@gmail.com>
2025-04-08 23:44:26 +0530

    added cli interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,68 @@
-#include <iostream>
 #include "db.hpp"
+
+#include <iostream>
+#include <string>
+#include <sstream>
 
 int main(){
     DBImpl db;
+    std::string line;
 
-    std::cout << "betaDB starting..." << std::endl;
-    
-    db.Set("user1", "name", "Shashidhar");
-    db.Set("user1", "age", "20");
-    db.Set("user1", "color", "black");
-    
-    // std::cout << "Name: " << *(db.Get("user1", "name")) << std::endl;
-   
-    std::vector<std::string> res = db.FetchByPrefix("user1", "age");
-    for(const auto& x: res){
-        std::cout << x << std::endl;
+    std::cout << "Welcome to BetaDB!" << std::endl;
+    while (true){
+        std::cout << ">> ";
+        std::getline(std::cin, line);
+        std::istringstream iss(line);
+        std::string cmd;
+        iss >> cmd;
+
+        if(cmd == "SET"){
+            std::string key, field, value;
+            int ttl = -1;
+            iss >> key >> field >> value >> ttl;
+            if(ttl > 0){
+                db.Set(key, field, value, ttl);
+            } else {
+                db.Set(key, field, value, std::nullopt);
+            }
+            std::cout << "OK\n";
+        } else if (cmd == "GET"){
+            std::string key, field;
+            iss >> key >> field;
+            auto res = db.Get(key, field);
+            if (res.has_value()){    
+                std::cout << *res << "\n";
+            }
+            else {
+                std::cout << "(null)\n";
+            }
+        } else if (cmd == "DELETE"){
+            std::string key, field;
+            iss >> key, field;
+            std::cout << (db.Delete(key, field)? "Deleted\n": "Not found\n");
+        } else if (cmd == "FETCH"){
+            std::string key;
+            iss >> key;
+            auto list = db.Fetch(key);
+            for(const auto& item: list){
+                std::cout << item << std::endl;
+            }
+        } else if (cmd == "PREFIX"){
+            std::string key, prefix;
+            iss >> key, prefix;
+            auto list = db.FetchByPrefix(key, prefix);
+            for(const auto& item: list){
+                std::cout << item << std::endl;
+            }
+        } else if (cmd == "EXIT"){
+            std::cout << "Thank you for using BetaDB!";
+            break;
+        } else if (cmd == "HELP"){
+            std::cout << "Commands:\n1. SET\n2. GET\n3. DELETE\n4. FETCH\n5. PREFIX\n6. EXIT\n7. HELP\nThank you!";
+        }
+        else {
+            std::cout << "unknown command. Type HELP for Manual.\n";
+        }
     }
-
-    return 0;
+    
 }
